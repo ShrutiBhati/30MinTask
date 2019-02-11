83dfb153501972b5dee57bb4ab88f63e6b3b6165
ShrutiBhati <shrutibhati96@gmail.com>
2019-02-11 23:27:55 +0530

    Updated tictactoe with backend

---
+++ ShrutiBhati016/Tictactoe/Controller/RecordsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Tictactoe.Model;
+
+namespace Tictactoe.Controller
+{
+    public class RecordsController : ApiController
+    {
+        private tictactoeEntities db = new tictactoeEntities();
+
+        // GET: api/Records
+        public IQueryable<Record> GetRecords()
+        {
+            return db.Records;
+        }
+
+        // GET: api/Records/5
+        [ResponseType(typeof(Record))]
+        public IHttpActionResult GetRecord(int id)
+        {
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(record);
+        }
+
+        // PUT: api/Records/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRecord(int id, Record record)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != record.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(record).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecordExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Records
+        [ResponseType(typeof(Record))]
+        public IHttpActionResult PostRecord(Record record)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Records.Add(record);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (RecordExists(record.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = record.Id }, record);
+        }
+
+        // DELETE: api/Records/5
+        [ResponseType(typeof(Record))]
+        public IHttpActionResult DeleteRecord(int id)
+        {
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            db.Records.Remove(record);
+            db.SaveChanges();
+
+            return Ok(record);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RecordExists(int id)
+        {
+            return db.Records.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ ShrutiBhati016/Tictactoe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tictactoe.WebApiApplication" Language="C#" %>
